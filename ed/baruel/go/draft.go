3a28e5993299e4b0e54ae97d480e2438b5abb828
Ariel Andrade <arielandradeadv@gmail.com>
2026-03-22 21:36:15 -0300

    (Baruel) Completa

--- ed/baruel/go/draft.go
+++ ed/baruel/go/draft.go
@@ -4,13 +4,14 @@ func main() {
     var totalAlbum, possui int
     fmt.Scan(&totalAlbum, &possui)
 
+    // Parte 1
     figurinhas := make([]int, possui)
     for i := range figurinhas {
         fmt.Scan(&figurinhas[i])
     }
     
     var repetidas []int
-    for i := 0; i < len(figurinhas)-1; i++ {
+    for i :=  0; i < len(figurinhas)-1; i++ {
         if figurinhas[i] == figurinhas[i+1]{
             repetidas = append(repetidas, figurinhas[i])
         }
@@ -27,6 +28,32 @@ func main() {
             }
         }
     }
+    fmt.Println() 
+
+    // Parte 2
+    album := make([]bool, totalAlbum+1)
+    for _, fig := range figurinhas {
+        album[fig] = true
+    }
+
+    var faltantes []int
+    for i := 1; i <= totalAlbum; i++ {
+        if !album[i] {
+            faltantes = append(faltantes, i)
+        }
+    }
+
+    if len(faltantes) == 0 {
+        fmt.Print("N")
+    } else {
+        for i := range faltantes {
+            fmt.Print(faltantes[i])
+
+            if i < len(faltantes)-1 {
+                fmt.Print(" ")
+            }
+        }
+    }
     fmt.Println()
-    
+
 }
